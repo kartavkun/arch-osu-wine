03b631b842d500e87a630d7d25cee50383240c33
kartavkun <nikitarobezhko@yandex.ru>
2024-09-30 12:38:36 +0500

    Add choice to add devserver shortcuts && fix some bugs

--- setup.sh
+++ setup.sh
@@ -114,14 +114,47 @@ FONTS_DIR="$FILES_DIR/fonts"
 CONFIG_DIR="$FILES_DIR/config"
 HOME_CONFIG_DIR="$HOME/.config"
 UDEV_RULES_DIR="/etc/udev/rules.d"
+DEVSERVER_DIR="$HOME/ArchOsu/files/devserver"
 
+add_akatsuki() {
+  cp $DEVSERVER_DIR/akatsuki/osu-akatsuki $DOT_LOCAL_DIR/bin
+  cp $DEVSERVER_DIR/akatsuki/osu-akatsuki.desktop $DOT_LOCAL_DIR/share/applications
+  chmod +x $HOME/.local/bin/osu-akatsuki
+  chmod +x $HOME/.local/share/applications/osu-akatsuki.desktop
+}
+
+add_gatari() {
+  cp $DEVSERVER_DIR/gatari/osu-gatari $DOT_LOCAL_DIR/bin
+  cp $DEVSERVER_DIR/gatari/osu-gatari.desktop $DOT_LOCAL_DIR/share/applications
+  chmod +x $HOME/.local/bin/osu-gatari
+  chmod +x $HOME/.local/share/applications/osu-gatari.desktop
+}
 # Make start file for osu!
 cp $FILES_DIR/.local/osu $DOT_LOCAL_DIR/bin
-chmod +x "$HOME/.local/bin/osu"
+chmod +x $HOME/.local/bin/osu
 
 # Make .desktop file for rofi/wofi
-cp $FILES_DIR/.local/osu.desktop $DOT_LOCAL_DIR/share
-chmod +x "$HOME/.local/share/applications/osu.desktop"
+cp $FILES_DIR/.local/osu.desktop $DOT_LOCAL_DIR/share/applications
+chmod +x $HOME/.local/share/applications/osu.desktop
+
+# Add devservers
+read -p "Do you want to add osu! shortcut for Akatsuki? (y/n) [по умолчанию n]: " choice
+choice=${choice:-n}  # Устанавливаем значение по умолчанию на 'n', если пользователь нажал Enter
+
+if [[ "$choice" == "y" ]]; then
+    add_akatsuki
+else
+    echo "Okay"
+fi
+
+read -p "Do you want to add osu! shortcut for Gatari? (y/n) [по умолчанию n]: " choice
+choice=${choice:-n}  # Устанавливаем значение по умолчанию на 'n', если пользователь нажал Enter
+
+if [[ "$choice" == "y" ]]; then
+    add_gatari
+else
+    echo "Okay"
+fi
 
 # Функция для копирования шрифтов
 echo "Installing fonts from Windows..."
