beb684f0adc5b91a453201c9bb58746596ca3477
kartavkun <nikitarobezhko@yandex.ru>
2024-10-31 21:04:15 +0500

    a

--- setup.sh
+++ setup.sh
@@ -47,10 +47,10 @@ fi
 yay -S --needed --noconfirm opentabletdriver osu-handler osu-mime tosu
 
 # Fix OTD
-echo "blacklist wacom" | sudo tee -a /etc/modprobe.d/blacklist.conf
-sudo rmmod wacom
-echo "blacklist hid_uclogic" | sudo tee -a /etc/modprobe.d/blacklist.conf
-sudo rmmod hid_uclogic
+# Regenerate initramfs
+sudo mkinitcpio -P
+# Unload kernel modules
+sudo rmmod wacom hid_uclogic
 systemctl --user daemon-reload
 systemctl --user enable opentabletdriver --now
 
