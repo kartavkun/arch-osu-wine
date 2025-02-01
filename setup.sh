9f98b43576bff5eb4b573afcb98202a9f6c17585
Kartav__ <69749101+kartavkun@users.noreply.github.com>
2025-02-01 18:38:56 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -55,7 +55,7 @@ systemctl --user daemon-reload
 systemctl --user enable opentabletdriver --now
 
 # Proton-osu mirrors
-PROTONLINK="https://github.com/whrvt/umubuilder/releases/download/proton-osu-9-13/proton-osu-9-13.tar.xz"
+PROTONLINK="https://github.com/whrvt/umubuilder/releases/download/proton-osu-9-14/proton-osu-9-14.tar.xz"
 WINEPREFIX="https://gitlab.com/NelloKudo/osu-winello-prefix/-/raw/master/osu-winello-prefix.tar.xz"
 
 # Make directories
@@ -67,11 +67,11 @@ mkdir -p "$HOME/.local/share/wineprefixes"
 mkdir -p "$HOME/.winellotmp"
 
 # Download Proton and Wineprefix
-wget -O "$HOME/.winellotmp/proton-osu-9-13.tar.xz" "$PROTONLINK" && chk="$?"
+wget -O "$HOME/.winellotmp/proton-osu-9-14.tar.xz" "$PROTONLINK" && chk="$?"
 wget -O "$HOME/.winellotmp/osu-winello-prefix-umu.tar.xz" "$WINEPREFIX" && chk="$?" 
 
 # Extract Proton
-tar -xf "$HOME/.winellotmp/proton-osu-9-13.tar.xz" -C "$HOME/.local/share/osuconfig"
+tar -xf "$HOME/.winellotmp/proton-osu-9-14.tar.xz" -C "$HOME/.local/share/osuconfig"
 
 # Extract Wineprefix
 tar -xf "$HOME/.winellotmp/osu-winello-prefix-umu.tar.xz" -C "$HOME/.local/share/wineprefixes"
