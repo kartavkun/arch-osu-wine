613d4a6fc98e7d1b416cc5c8fed9e2bdfe9b96a5
kartavkun <nikitarobezhko@yandex.ru>
2025-02-20 15:34:32 +0500

    another fix

--- scripts/fonts.sh
+++ scripts/fonts.sh
@@ -2,8 +2,9 @@
 
 mkdir -p $HOME/tmp
 FONTS_DIR="$HOME/tmp"
+FONT_TAR="https://gitlab.com/kartavkun/arch-osu-wine-extras/-/raw/main/WindowsFonts.tar.xz"
 
-wget -O $FONTS_DIR/WindowsFonts.tar.xz https://gitlab.com/kartavkun/arch-osu-wine-extras/-/raw/main/WindowsFonts.tar.xz
+wget -O "$FONTS_DIR/WindowsFonts.tar.xz" "$FONT_TAR" && chk="$?"
 tar -xf $FONTS_DIR/WindowsFonts.tar.xz -C $FONTS_DIR
 
 # Функция для копирования шрифтов
