4e3bd68b71ff0d5c04adb4191be193c3542e3c6b
kartavkun <nikitarobezhko@yandex.ru>
2025-02-20 15:44:59 +0500

    another fkin fix. i'm tired

--- scripts/fonts.sh
+++ scripts/fonts.sh
@@ -5,7 +5,7 @@ FONTS_DIR="$HOME/tmp"
 FONT_TAR="https://gitlab.com/kartavkun/arch-osu-wine-extras/-/raw/main/WindowsFonts.tar.xz"
 
 wget -O "$FONTS_DIR/WindowsFonts.tar.xz" "$FONT_TAR" && chk="$?"
-tar -xf $FONTS_DIR/WindowsFonts.tar.xz -C $FONTS_DIR
+tar -xf $FONTS_DIR/WindowsFonts.tar.xz -C "$FONTS_DIR"
 
 # Функция для копирования шрифтов
 echo "Installing fonts from Windows..."
@@ -15,4 +15,4 @@ sudo fc-cache --force
 sudo fc-cache-32 --force
 echo "The fonts successfully installed."
 
-rm -rf $FONTS_DIR/*
+rm -rf $HOME/tmp
