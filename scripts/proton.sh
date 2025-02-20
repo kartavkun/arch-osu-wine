7f70583356f743b672fb869354ac5e4f401472b1
kartavkun <nikitarobezhko@yandex.ru>
2025-02-20 16:37:16 +0500

    maybe last

--- scripts/proton.sh
+++ scripts/proton.sh
@@ -2,14 +2,16 @@
 
 mkdir -p "$HOME/tmp"
 mkdir -p "$HOME/.local/share"
+mkdir -p "$HOME/.local/share/osuconfig"
+mkdir -p "$HOME/.local/share/wineprefixes"
 
 # Proton-osu mirrors
 PROTONLINK="https://github.com/whrvt/umubuilder/releases/download/proton-osu-9-15/proton-osu-9-15.tar.xz"
 WINEPREFIX="https://gitlab.com/NelloKudo/osu-winello-prefix/-/raw/master/osu-winello-prefix.tar.xz"
 
 # Download Proton and Wineprefix
-wget -O "$HOME/tmp/proton-osu-9-15.tar.xz" "$PROTONLINK" && chk="$?"
-wget -O "$HOME/tmp/osu-winello-prefix-umu.tar.xz" "$WINEPREFIX" && chk="$?"
+wget -O "$HOME/tmp/proton-osu-9-15.tar.xz" "$PROTONLINK"
+wget -O "$HOME/tmp/osu-winello-prefix-umu.tar.xz" "$WINEPREFIX"
 
 # Extract Proton
 tar -xf "$HOME/tmp/proton-osu-9-15.tar.xz" -C "$HOME/.local/share/osuconfig"
