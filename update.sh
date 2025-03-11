8f579e2fe3e4a35a81b0a2ddbe0dc15d09d75b44
Kartav__ <69749101+kartavkun@users.noreply.github.com>
2025-03-11 10:32:00 +0000

    delete checkout to dev branch

--- update.sh
+++ update.sh
@@ -25,9 +25,8 @@ mkdir -p "$HOME/tmp/"
 
 # Delete old repo
 rm -rf $HOME/osuinstall
-git clone https://github.com/kartavkun/arch-osu-wine.git $HOME/osuinstall
+git clone --depth=1 https://github.com/kartavkun/arch-osu-wine.git $HOME/osuinstall
 cd $HOME/osuinstall
-git checkout dev
 
 clear
 $SCRIPT/proton.sh
